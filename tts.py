b01be32519514867ff96a086f85ecb42791632c5
Glenn Velupillai <glennsvelu@gmail.com>
2020-06-23 19:10:58 -0400

    Update tts.py

--- tts.py
+++ tts.py
@@ -6,3 +6,14 @@ f = codecs.open("booktext.txt", encoding='utf-8')
 cnt = 0
 file_names = ''
 
+for line in f:
+    rendered = ''
+    line = line.replace('"', '\\"')
+    command = 'aws polly synthesize-speech --text-type ssml --output-format "mp3" --voice-id "Kendra" --text "{0}" {1}'
+
+    if '\r\n' == line:
+        #A pause after a paragraph
+        rendered = '<speak><break time= "2s"/></speak>'
+    else:
+        #A pause after a sentence
+        rendered = '<speak><amazon:effect name=\\"drc\\">' + line.strip() + '<break time=\\"1s\\"/></amazon:effect></speak>'
